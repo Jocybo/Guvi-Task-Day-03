6feffe31a12042a7ffc57f19ad79b375710f7bca
jocybo <naveencybosec@gmail.com>
2022-01-24 20:50:28 +0530

    resume data

---
+++ resume.js
@@ -0,0 +1,18 @@
+let resumeData = {
+    "name":"Naveen Raj",
+    "DOB":"15/06/2000",
+    "mail":"naveencybosec@gmail.com",
+    "qualification":"B.sc Information Technology",
+    "certifications":["HTML","CSS","JavaScript","Python"],
+    "academic":{
+        "UG":"75%",
+        "HSC":"67%",
+        "SSLC":"84%",
+    },
+    "areaofinterest":["FullStack","Cybersecurity"],
+    "college":"srinivasan college of arts and science perambalur",
+    "languageknown":["english","tamil"],
+    "hobbies":["reading","cooking"],
+    "personalskills":["fast learner","team management","easy to adapt"],
+}
+console.log(resumeData);
